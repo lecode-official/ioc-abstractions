6b2eaf038820e757e11d4a34169917b3e0b3a7a5
David Neumann <david.neumann@lecode.de>
2016-02-04 13:12:27 +0100

    Initial commit of the abstraction interfaces for inversion of control containers.

---
+++ System.InversionOfControl.Abstractions/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿
+#region Using Directives
+
+using System.Resources;
+using System.Reflection;
+
+#endregion
+
+#region General Assembly Information
+
+[assembly: AssemblyTitle("IoC Abstractions")]
+[assembly: AssemblyDescription("Represents an abstraction layer for multiple IoC containers. Using the abstractions, applications can use an inversion of control container for dependency injection, without directly seeing the underlying API. This makes it very easy to switch between different IoC implementations.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IoC Abstractions")]
+[assembly: AssemblyCopyright("Copyright © David Neumann & Lukas Rögner 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+#endregion
+
+#region Assembly Version Information
+
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+#endregion
