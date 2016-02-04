787ac25cf49d0cb9810af435720654b61661e257
David Neumann <david.neumann@lecode.de>
2016-02-04 18:01:49 +0100

    Added a sample application, which showcases the abilities of the IoC Abstractions framework.

---
+++ Samples/IocAbstractions.Samples.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿
+#region Using Directives
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+#region General Assembly Information
+
+[assembly: AssemblyTitle("IocAbstractions Console Sample")]
+[assembly: AssemblyDescription("A console application, which showcases the abilities of the IoC Abstractions framework.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IoC Abstractions")]
+[assembly: AssemblyCopyright("Copyright © David Neumann & Lukas Rögner 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4ab43f60-f2fd-43e4-b33c-3927d8ffa9f2")]
+
+#endregion
+
+#region Assembly Version Information
+
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+#endregion
