65e0a1d969e0d0569cedf11c559cd74b3bfac749
David Neumann <david.neumann@live.com>
2016-02-04 13:12:27 +0100

    Initial commit of the abstraction interfaces for inversion of control containers.

---
+++ System.InversionOfControl.Abstractions/IIocContainer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace System.InversionOfControl.Abstractions
+{
+    /// <summary>
+    /// Represents an abstraction layer for inversion of control containers.
+    /// </summary>
+    public interface IIocContainer : IReadOnlyIocContainer
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the name of the inversion of control container.
+        /// </summary>
+        string Name { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Registers a type and binds it to itself in the specified scope.
+        /// </summary>
+        /// <typeparam name="T">The type that is to be registered.</typeparam>
+        /// <param name="scope">The scope in which the type should be resolved.</param>
+        void RegisterType<T>(Scope scope = Scope.Transient);
+
+        /// <summary>
+        /// Registers a type and binds it to itself in the specified scope.
+        /// </summary>
+        /// <typeparam name="T">The type that is to be registered.</typeparam>
+        /// <param name="whenInjectedInto">The type is only resolved when it is injected into this type.</param>
+        /// <param name="onlyInjectExactlyInto">Determines whether the type is resolved when being injected into the specified type and its sub-types only only when being injected into the specified type.</param>
+        /// <param name="scope">The scope in which the type should be resolved.</param>
+        void RegisterType<T>(Type whenInjectedInto, bool onlyInjectExactlyInto = false, Scope scope = Scope.Transient);
+
+        /// <summary>
+        /// Registers a type and binds it to the specified type in the specified scope.
+        /// </summary>
+        /// <typeparam name="T">The type that is to be registered.</typeparam>
+        /// <typeparam name="U">The type to which the registered type is being bound.</typeparam>
+        /// <param name="scope">The scope in which the type should be resolved.</param>
+        void RegisterType<T, U>(Scope scope = Scope.Transient);
+
+        /// <summary>
+        /// Registers a type and binds it to the specified type in the specified scope.
+        /// </summary>
+        /// <typeparam name="T">The type that is to be registered.</typeparam>
+        /// <typeparam name="U">The type to which the registered type is being bound.</typeparam>
+        /// <param name="whenInjectedInto">The type is only resolved when it is injected into this type.</param>
+        /// <param name="onlyInjectExactlyInto">Determines whether the type is resolved when being injected into the specified type and its sub-types only only when being injected into the specified type.</param>
+        /// <param name="scope">The scope in which the type should be resolved.</param>
+        void RegisterType<T, U>(Type whenInjectedInto, bool onlyInjectExactlyInto = false, Scope scope = Scope.Transient);
+
+        /// <summary>
+        /// Registers a type and binds it to the specified factory method in the specified scope.
+        /// </summary>
+        /// <typeparam name="T">The type that is to be registered.</typeparam>
+        /// <param name="factory">The factory method that resolves the registered type.</param>
+        /// <param name="scope">The scope in which the type should be resolved.</param>
+        void RegisterType<T>(Func<T> factory, Scope scope = Scope.Transient);
+
+        /// <summary>
+        /// Registers a type and binds it to the specified factory method in the specified scope.
+        /// </summary>
+        /// <typeparam name="T">The type that is to be registered.</typeparam>
+        /// <param name="factory">The factory method that resolves the registered type.</param>
+        /// <param name="whenInjectedInto">The type is only resolved when it is injected into this type.</param>
+        /// <param name="onlyInjectExactlyInto">Determines whether the type is resolved when being injected into the specified type and its sub-types only only when being injected into the specified type.</param>
+        /// <param name="scope">The scope in which the type should be resolved.</param>
+        void RegisterType<T>(Func<T> factory, Type whenInjectedInto, bool onlyInjectExactlyInto = false, Scope scope = Scope.Transient);
+
+        #endregion
+    }
+}
---
+++ System.InversionOfControl.Abstractions/IReadOnlyIocContainer.cs
@@ -0,0 +1,43 @@
+﻿
+namespace System.InversionOfControl.Abstractions
+{
+    /// <summary>
+    /// Represents an abstraction layer for inversion of control containers, which can only resolve instances but not register types.
+    /// </summary>
+    public interface IReadOnlyIocContainer
+    {
+        #region Methods
+
+        /// <summary>
+        /// Resolves an instance of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type that is to be resolved.</typeparam>
+        /// <returns>Returns a resolved instance of the specified type.</returns>
+        T GetInstance<T>();
+
+        /// <summary>
+        /// Resolves an instance of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type that is to be resolved.</typeparam>
+        /// <param name="constructorParameters">Constructor parameters, which are preferred over registered types.</param>
+        /// <returns>Returns a resolved instance of the specified type.</returns>
+        T GetInstance<T>(params object[] constructorParameters);
+
+        /// <summary>
+        /// Resolves an instance of the specified type.
+        /// </summary>
+        /// <param name="typeToResolve">The type that is to be resolved.</param>
+        /// <returns>Returns a resolved instance of the specified type.</returns>
+        object GetInstance(Type typeToResolve);
+
+        /// <summary>
+        /// Resolves an instance of the specified type.
+        /// </summary>
+        /// <param name="typeToResolve">The type that is to be resolved.</param>
+        /// <param name="constructorParameters">Constructor parameters, which are preferred over registered types.</param>
+        /// <returns>Returns a resolved instance of the specified type.</returns>
+        object GetInstance(Type typeToResolve, params object[] constructorParameters);
+
+        #endregion
+    }
+}
