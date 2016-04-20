b55a5d152852c8a25c4dbb0d9a23dbb4286f6a80
David Neumann <david.neumann@lecode.de>
2016-04-20 11:41:46 +0200

    Bumbed up the version number and made the projects ready for release on NuGet.

--- System.InversionOfControl.Abstractions/Properties/AssemblyInfo.cs
+++ System.InversionOfControl.Abstractions/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Reflection;
 
 #region Assembly Version Information
 
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.2.*")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
 
 #endregion
